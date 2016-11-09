51da872ddee0dfb661ad2b9de76692b4b56617af
Tony <1024303678@qq.com>
2016-11-09 21:28:43 +0800

    CoolWeather

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,33 @@
+package com.coolweather.app.model;
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provicneCode;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getProvinceName() {
+        return provinceName;
+    }
+    
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    
+    public String getProvicneCode() {
+        return provicneCode;
+    }
+    
+    public void setProvicneCode(String provicneCode) {
+        this.provicneCode = provicneCode;
+    }
+    
+}
